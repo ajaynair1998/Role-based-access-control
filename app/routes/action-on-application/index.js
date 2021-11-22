79337abf079c7a4a120bf8344c566976a2da8118
AJAY <ajaynairarun@gmail.com>
2021-11-22 23:21:19 +0530

    add middle-ware-to-all-privilage-only-routes

--- app/routes/action-on-application/index.js
+++ app/routes/action-on-application/index.js
@@ -1,10 +1,11 @@
 let express = require("express");
 // import the db connection class
 let Connection = require("../../database/connection");
+const checkPrivilege = require("../../middlewares/check-privilege");
 let db = new Connection();
 let router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", checkPrivilege, async (req, res) => {
   let { id, newState } = req.body;
   let completedSuccessfully = await db.changeStateOfApplication(id, newState);
   console.log(id, newState);
